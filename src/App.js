f7e9552be0b1d4906c6ac44af3559a7ce288bcc8
Hasnain-Raza-Gaho <gahohasnainraza@gmail.com>
2022-02-02 11:53:27 +0500

    'done'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          New Website By Hasnain Raza Gaho
+        all things done here
         </p>
         <a
           className="App-link"
